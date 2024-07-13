282c9d2a1cdec52e94bb7760480538cfc0c797ed
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2024-07-13 11:34:33 -0300

    Add files via upload

---
+++ MFPD-Treadmill/fxmanifest.lua
@@ -0,0 +1,20 @@
+--********************************************************************************--
+--*                                                                              *--
+--*   MFPD-Treadmill - SYNCED ANIMATION AND SOUND EFFECTS TO ALL PLAYERS!         *--
+--*                                                                              *--
+--********************************************************************************--
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+    'html/sounds/andar.ogg',
+    'html/sounds/correr.ogg',
+}
+
+
